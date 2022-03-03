61745e7a11d130d41433764bf60006240668552c
lgzzk <zzk171@outlook.com>
2022-03-03 22:59:51 +0800

    提交了更新

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,30 @@
 <template>
   <div id="app">
-
+    <Header/>
+    <Navigation/>
+    <Content/>
   </div>
 </template>
 
 <script>
 
+import Header from "@/components/Header";
+import Navigation from "@/components/Navigation";
+import Content from "@/components/Content";
 export default {
   name: 'App',
   components: {
+    Content,
+    Navigation,
+    Header
   }
 }
 </script>
 
 <style>
-
+  #app{
+    width: 100vh;
+    height: 100vh;
+    overflow: hidden;
+  }
 </style>
--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -11,5 +11,12 @@ export default {
 </script>
 
 <style scoped>
-
+  #navigation{
+    width: 300px;
+    height: 100%;
+    left: 0px;
+    z-index: 10;
+    position: absolute;
+    background-color: #fefefe;
+  }
 </style>
