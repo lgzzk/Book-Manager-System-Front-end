f47772fbbf772514aa9cbd8b6326609095b6951d
lgzzk <zzk171@outlook.com>
2022-03-03 22:59:51 +0800

    提交了更新

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -11,5 +11,10 @@ export default {
 </script>
 
 <style scoped>
-
+  #content{
+    height: 100%;
+    width: 100%;
+    position: absolute;
+    background-color: #E9ECF3;
+  }
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="header">
-
+    <span>图书管理系统</span>
   </div>
 </template>
 
@@ -11,5 +11,19 @@ export default {
 </script>
 
 <style scoped>
+  #header{
+    width: 100%;
+    height: 100px;
+    z-index: 10;
+    color: #999;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: absolute;
+    background-color: #fefefe;
+  }
 
+  span{
+    font-size: 50px;
+  }
 </style>
